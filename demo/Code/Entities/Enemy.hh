f9c9fece0e5497cc61181fadf3cc0bed93c56c96
iWas-Coder <wasymatieh01@gmail.com>
2026-02-22 18:35:28 +0100

    There was no business maintaining yet another demo project... maybe later

--- demo/Code/Entities/Enemy.hh
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-// Copyright (C) Wasym A. Alonso. All Rights Reserved.
-
-struct Enemy final : Character<Color::Orange, 40> {
-  virtual void Update(const f64 dt) override {
-    m_Position += 30 * dt;
-  }
-};
